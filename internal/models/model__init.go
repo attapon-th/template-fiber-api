666788e05f3dcb01f20ae4e1d6e67d48c150e470
Attapon Thanawong <attapon_srem@hotmail.com>
2023-11-10 15:35:49 +0700

    add null,ksuid

--- internal/models/model__init.go
+++ internal/models/model__init.go
@@ -1,2 +1,19 @@
 // Package models create model
 package models
+
+import (
+	"github.com/attapon-th/null"
+	"github.com/segmentio/ksuid"
+)
+
+// ModelID primary key
+type ModelID struct {
+	ID       ksuid.KSUID `json:"id"`
+	LastUser null.String `json:"last_user"`
+}
+
+// NewID create new ID
+func (m *ModelID) NewID() ksuid.KSUID {
+	m.ID = ksuid.New()
+	return m.ID
+}
